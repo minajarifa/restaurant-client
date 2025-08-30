66d8c873f7757177427465847deb3868efb5cf4b
Your Name <minajmiha02@gmail.com>
2025-08-30 23:46:03 +0600

    main

---
+++ src/router.jsx
@@ -0,0 +1,19 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import App from "./App";
+import Home from "./Pages/Home/Home";
+
+export   const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+      children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
