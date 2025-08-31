59bb934630ce84654a0d2a7ec17068c65546d102
Your Name <minajmiha02@gmail.com>
2025-08-31 07:52:23 +0600

    added div for header footer

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,10 +1,19 @@
 import { Outlet } from "react-router-dom";
-
+import Header from "../Pages/Shared/Header/Header";
+import Footer from "../Pages/Shared/Footer/Footer";
 
 export default function Main() {
   return (
     <>
-    <Outlet></Outlet>
+      <div>
+        <Header />
+      </div>
+      <div>
+        <Outlet></Outlet>
+      </div>
+      <div>
+        <Footer />
+      </div>
     </>
-  )
+  );
 }
