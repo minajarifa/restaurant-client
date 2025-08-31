9f45cf7c48023246daf06d8f7be72f60b7911090
Your Name <minajmiha02@gmail.com>
2025-08-31 22:33:25 +0600

    category added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "./Banner/Banner";
+import Category from "./Category/Category";
 
 
 export default function Home() {
   return (
     <div>
       <Banner/>
+      <Category/>
     </div>
   )
 }
