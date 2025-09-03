ca5cca0a20fa3460df71dceff4abf414cdc6a64d
Your Name <minajmiha02@gmail.com>
2025-09-03 18:30:33 +0600

    menuItem updated

--- src/Pages/Home/Banner/PopularManu/PopularManu.jsx
+++ src/Pages/Home/Banner/PopularManu/PopularManu.jsx
@@ -18,7 +18,7 @@ export default function PopularManu() {
   return (
     <div>
         <Title heading={`From our manu`} subHeading={`Popular Items`}/>
-        <div>
+        <div className="grid grid-cols-2 gap-4">
             {
                 menu.map(item=><MenuItem key={item._id} item={item}></MenuItem>)
             }
--- src/Pages/Shared/ManuItem/MenuItem.jsx
+++ src/Pages/Shared/ManuItem/MenuItem.jsx
@@ -3,6 +3,13 @@ import React from 'react'
 export default function MenuItem({item}) {
     console.log(item)
   return (
-    <div>MenuItem</div>
+    <div className='flex space-x-4'>
+        <img style={{borderRadius:'0 200px 200px 200px'}} className='w-[120px]' src={item.image} alt="" />
+        <div>
+            <h3 className='uppercase'>{item.name}------</h3>
+            <p>{item.recipe}</p>
+        </div>
+        <p className='text-yellow-500'>${item.price}</p>
+    </div>
   )
 }
