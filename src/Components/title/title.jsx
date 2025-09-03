83ecc0b951dc02f47a5f378f4a16ce99bc2601f6
Your Name <minajmiha02@gmail.com>
2025-09-03 10:06:31 +0600

    title

--- src/Components/title/title.jsx
+++ src/Components/title/title.jsx
@@ -1,7 +1,8 @@
-export default function title() {
+export default function title({heading, subHeading}) {
   return (
-    <div>
-        
+    <div className="mx-auto md:w-3/12">
+        <p className="">{heading}</p>
+        <p className="">{subHeading}</p>
     </div>
   )
 }
--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,22 +1,23 @@
 import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css"
-import img1 from "../../../../public/images/imagesBanner/img1.jpeg"
-import img2 from "../../../../public/images/imagesBanner/img2.jpeg"
-import img3 from "../../../../public/images/imagesBanner/img3.jpeg"
-import img4 from "../../../../public/images/imagesBanner/img4.jpeg"
-import img5 from "../../../../public/images/imagesBanner/img5.jpeg"
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import img1 from "../../../../public/images/imagesBanner/img1.jpeg";
+import img2 from "../../../../public/images/imagesBanner/img2.jpeg";
+import img3 from "../../../../public/images/imagesBanner/img3.jpeg";
+import img4 from "../../../../public/images/imagesBanner/img4.jpeg";
+import img5 from "../../../../public/images/imagesBanner/img5.jpeg";
 
 export default function Banner() {
   return (
-    <div className="h-auto">
+    <>
+      {/* <div className="">
       <Carousel
         showArrows={true}
         // onChange={onChange}
         // onClickItem={onClickItem}
         // onClickThumb={onClickThumb}
       >
-        <div >
-          <img  src={img1}/>
+        <div className="min-h-screen ">
+          <img src={img1}/>
           <p className="legend">Legend 1</p>
         </div>
         <div>
@@ -37,6 +38,32 @@ export default function Banner() {
         </div>
         
       </Carousel>
-    </div>
+    </div> */}
+      <div className="">
+        <Carousel showArrows={true}>
+          <div className="h-[400px]">
+            {/* ekhane fixed height */}
+            <img src={img1} className="object-cover w-full h-full" />
+            <p className="legend">Legend 1</p>
+          </div>
+          <div className="h-[400px]">
+            <img src={img2} className="object-cover w-full h-full" />
+            <p className="legend">Legend 2</p>
+          </div>
+          <div className="h-[400px]">
+            <img src={img3} className="object-cover w-full h-full" />
+            <p className="legend">Legend 3</p>
+          </div>
+          <div className="h-[400px]">
+            <img src={img4} className="object-cover w-full h-full" />
+            <p className="legend">Legend 4</p>
+          </div>
+          <div className="h-[400px]">
+            <img src={img5} className="object-cover w-full h-full" />
+            <p className="legend">Legend 5</p>
+          </div>
+        </Carousel>
+      </div>
+    </>
   );
 }
