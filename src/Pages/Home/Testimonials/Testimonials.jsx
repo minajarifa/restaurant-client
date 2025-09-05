8553f5a15602b92ee3eda669f6807686a9edf5c9
Your Name <minajmiha02@gmail.com>
2025-09-05 19:33:12 +0600

    testimonials

--- src/Pages/Home/Testimonials/Testimonials.jsx
+++ src/Pages/Home/Testimonials/Testimonials.jsx
@@ -4,6 +4,8 @@ import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper/modules";
 import { useEffect, useState } from "react";
+import { Rating } from "@smastrom/react-rating";
+import "@smastrom/react-rating/style.css";
 
 export default function Testimonials() {
   const [reviews, setReviews] = useState([]);
@@ -19,10 +21,16 @@ export default function Testimonials() {
     <section>
       <Title subHeading={"what our client say"} heading={"Testimonials"} />
       <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-       reviews:  {reviews?.length}
+        reviews: {reviews?.length}
         <div>
           {reviews.map((review) => (
-            <SwiperSlide key={review?._id}>Slide 1</SwiperSlide>
+            <SwiperSlide key={review?._id}>
+              <div className="flex flex-col items-center mx-24 my-16">
+                <Rating style={{ maxWidth: 180 }} value={review.rating} readOnly />
+                <p className="py-8">{review.details}</p>
+                <h3 className="text-2xl text-orange-400">{review.name}</h3>
+              </div>
+            </SwiperSlide>
           ))}
         </div>
       </Swiper>
