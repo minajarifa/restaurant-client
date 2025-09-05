6975e92bc4b795a0eeb10d4266fc0ff4a1b45425
Your Name <minajmiha02@gmail.com>
2025-09-05 23:45:53 +0600

    menu item

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Cover from "../../Shared/Cover/Cover";
 import { Helmet } from "react-helmet-async";
+import PopularManu from "../../Home/PopularManu/PopularManu";
 
 export default function Menu() {
   return (
@@ -9,9 +10,13 @@ export default function Menu() {
         <title>Restaurant || Our Menu</title>
       </Helmet>
       <Cover title={`Pizza`}></Cover>
+      <PopularManu />
       <Cover title={`Burger`}></Cover>
+      <PopularManu />
       <Cover title={`kacci`}></Cover>
+      <PopularManu />
       <Cover title={`biriani`}></Cover>
+      <PopularManu />
       <div>menu</div>
     </div>
   );
