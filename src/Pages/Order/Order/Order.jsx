88fa3d01ced85ca2f4f5642cd0504a95ef7cf09f
Your Name <minajmiha02@gmail.com>
2025-09-06 22:25:26 +0600

    react tabs

--- src/Pages/Order/Order/Order.jsx
+++ src/Pages/Order/Order/Order.jsx
@@ -1,7 +1,23 @@
-
-
+import Cover from "../../Shared/Cover/Cover";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 export default function Order() {
   return (
-    <div>Order</div>
-  )
+    <div>
+      <Cover />
+      <Tabs>
+        <TabList>
+          <Tab>Title 1</Tab>
+          <Tab>Title 2</Tab>
+        </TabList>
+
+        <TabPanel>
+          <h2>Any content 1</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 2</h2>
+        </TabPanel>
+      </Tabs>
+    </div>
+  );
 }
