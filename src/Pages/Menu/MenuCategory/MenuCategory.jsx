bacc77ca47375b219f2cbd566ca3742e088ae213
Your Name <minajmiha02@gmail.com>
2025-09-06 15:46:13 +0600

    use menu

--- src/Pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/Pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,4 +1,16 @@
+import useMenu from "../../../hooks/useMenu";
+
 export default function MenuCategory() {
+      const [menu] = useMenu();
+  const popular = menu?.filter((item) => item.category === "popular");
+  const salad = menu?.filter((item) => item.category === "salad");
+  const drinks = menu?.filter((item) => item.category === "drinks");
+  const dessert = menu?.filter((item) => item.category === "dessert");
+  const pizza = menu?.filter((item) => item.category === "pizza");
+  const soup = menu?.filter((item) => item.category === "soup");
+  const offered = menu?.filter((item) => item.category === "offered");
+  const Haddock = menu?.filter((item) => item.category === "Haddock");
+  
   return (
     <div>
     
