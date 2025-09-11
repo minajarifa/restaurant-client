1d4a336bbabfc5ab1b632aeb9576ec59bed4f2e2
Your Name <minajmiha02@gmail.com>
2025-09-11 20:03:29 +0600

    added button

--- src/Components/FoodCard/FoodCard.jsx
+++ src/Components/FoodCard/FoodCard.jsx
@@ -11,12 +11,12 @@ export default function FoodCard({ item }) {
         />
       </figure>
       <p className="absolute right-0 px-4 mt-4 mr-4 text-white bg-slate-900">{price}</p>
-      <div className="items-center text-center card-body">
+      <div className="flex flex-col items-center card-body">
         <h2 className="card-title">{category}</h2>
         <h2 className="card-title">{name}</h2>
         <p>{recipe}</p>
         <div className="card-actions">
-          <button className="btn btn-primary">Buy Now</button>
+          <button className="btn btn-outline bg-slate-100">Buy Now</button>
         </div>
       </div>
     </div>
