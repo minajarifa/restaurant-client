3cb20b27dfca758553a2562ab74b8f42fa7bd590
Your Name <minajmiha02@gmail.com>
2025-09-16 18:48:45 +0600

    current usser

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,9 +1,21 @@
-import { createContext, useState } from "react";
+/* eslint-disable react-refresh/only-export-components */
+import { onAuthStateChanged } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import { auth } from "../firebase/firebase.config";
 
 export const AuthContext = createContext(null);
 export default function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
-  const authInfo = { user, setUser };
+  const [loading, setLoading] = useState(null);
+  useEffect(()=>{
+    onAuthStateChanged(auth,currentUser=>{
+        setUser(currentUser)
+        console.log("current User",currentUser)
+        setLoading(false)
+    })
+  },[])
+ 
+  const authInfo = { user,loading };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
