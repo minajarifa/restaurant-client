3b8b47d9871e65f351ec6df3cce1fc0d9faef6af
Your Name <minajmiha02@gmail.com>
2025-09-22 15:23:16 +0600

    axios

--- src/Components/FoodCard/FoodCard.jsx
+++ src/Components/FoodCard/FoodCard.jsx
@@ -1,22 +1,25 @@
 export default function FoodCard({ item }) {
   // console.log(item);
-  const {category,image,name,price,recipe,}=item;
+  const { category, image, name, price, recipe } = item;
+  const handleAddToCard = (food) => {
+    console.log(food);
+  };
   return (
     <div className="shadow-xl card w-96 bg-base-100">
       <figure className="px-10 pt-10">
-        <img
-          src={image}
-          alt="Shoes"
-          className="rounded-xl"
-        />
+        <img src={image} alt="Shoes" className="rounded-xl" />
       </figure>
-      <p className="absolute right-0 px-4 mt-4 mr-4 text-white bg-slate-900">{price}</p>
+      <p className="absolute right-0 px-4 mt-4 mr-4 text-white bg-slate-900">
+        {price}
+      </p>
       <div className="flex flex-col items-center card-body">
         <h2 className="card-title">{category}</h2>
         <h2 className="card-title">{name}</h2>
         <p>{recipe}</p>
         <div className="card-actions">
-          <button className="mt-4 border-0 border-b-4 border-orange-400 btn btn-outline ">Buy Now</button>
+          <button onClick={()=>handleAddToCard(item)} className="mt-4 border-0 border-b-4 border-orange-400 btn btn-outline ">
+            Buy Now
+          </button>
         </div>
       </div>
     </div>
